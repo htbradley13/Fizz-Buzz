bf18242feced4be277d84f5a75abe7c537cd8a3b
htbradley13 <htbradley13@gmail.com>
2016-08-11 21:12:36 -0400

    final

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	for (var startGame = 1; startGame <= 100; startGame++) {
+
+	if (startGame % 3 === 0) {
+		if (startGame % 5 === 0) {
+			document.write("Fizz Buzz" + "<br>");
+		}
+		else (document.write("Fizz" + "<br>"))
+	}
+	else if (startGame % 5 === 0) {
+		if (startGame % 3 != 0) {
+			document.write("Buzz" + "<br>");
+		}
+	}
+	else if (startGame % 5 != 0) {
+		if (startGame % 3 != 0) {
+			document.write(startGame + "<br>");
+		}
+	}
+	
+	}
+}); 
