e5722cc472f9f37de3aa5b2bfcf6afbea2e17a61
htbradley13 <htbradley13@gmail.com>
2016-08-14 15:57:44 -0400

    refraction

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 $(document).ready(function() {
 	for (var startGame = 1; startGame <= 100; startGame++) {
 
-	if (startGame % 3 === 0) {
-		if (startGame % 5 === 0) {
+	if (startGame % 3 === 0 && startGame % 5 === 0) {
 			document.write("Fizz Buzz" + "<br>");
 		}
-		else (document.write("Fizz" + "<br>"))
+	else if(startGame % 3 === 0 && startGame % 5 != 0) {
+		document.write("Fizz" + "<br>");
 	}
-	else if (startGame % 5 === 0) {
-		if (startGame % 3 != 0) {
-			document.write("Buzz" + "<br>");
-		}
+	else if(startGame % 5 === 0 && startGame % 3 != 0) {
+		document.write("Buzz" + "<br>");
 	}
-	else if (startGame % 5 != 0) {
-		if (startGame % 3 != 0) {
+	else {
 			document.write(startGame + "<br>");
 		}
-	}
-	
 	}
 });  
 //done 
